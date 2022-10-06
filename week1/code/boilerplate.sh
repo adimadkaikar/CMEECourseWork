c2e285aeac91d39b72ccdc944d063deebe3ff53d
adimadkaikar <arm122@ic.ac.uk>
2022-10-06 10:44:43 +0100

    Full second commit of new project

---
+++ week1/code/boilerplate.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# Author: arm122@ic.ac.uk
+# Script: boilerplate.sh
+# Desc: simple bioilerplate for shell scripting
+# Arguement: none
+# Date: Oct 2022
+
+echo -e "\nThis is a shell script"
+
+#exit
---
+++ week1/code/tabtocsv.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Author: Aditi Madkaikar arm122@ic.ac.uk
+#Script: tabtocsv.sh
+#Descripton: substitute all the tabs for commas
+#
+#Saves the output to a .csv file
+#Arguements: 1 -> tab delimited file
+#Date: Oct 2022
+
+echo  "Creating a comma delimited version of $1 ..."
+cat $1 | tr -s "\t" "," >> $1.csv
+echo "Done!"
+
+exit
