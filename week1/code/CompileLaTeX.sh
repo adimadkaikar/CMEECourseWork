d79500061a7235af4a987f46ac376fbc5380a054
adimadkaikar <arm122@ic.ac.uk>
2022-10-17 12:05:27 +0100

    Modified code

--- week1/code/CompileLaTeX.sh
+++ week1/code/CompileLaTeX.sh
@@ -6,19 +6,29 @@
 #Arguements: 1 -> Filename of the .tex form ideally without the .tex suffix
 #Date: Oct 2022
 
+
 if [[ $1 =~ .*tex.* ]]; then
-  echo "Please input a '.tex' file"
+    #echo "Please input a '.tex' file"
     pdflatex $1
     bibtex $(basename "$1" .tex)
     pdflatex $1
     pdflatex $1
     evince $(basename "$1" .tex).pdf
 else
-    pdflatex $1.tex
-    bibtex $1
-    pdflatex $1.tex
-    pdflatex $1.tex
-    evince $1.pdf
+    echo -n "Please enter a .tex file with the .tex extension (eg: MyExample.tex should be entered as MyExample.tex): "
+    read in_file
+    echo "$in_file"
+    if [[ $in_file =~ .*tex.* ]]; then
+      echo "Please input a '.tex' file"
+      pdflatex $in_file
+      bibtex $(basename "$in_file" .tex)
+      pdflatex $in_file
+      pdflatex $in_file
+      evince $(basename "$in_file" .tex).pdf
+    else
+      echo "Enter a valid input"
+      exit
+    fi
 fi
 
 #Cleanup
