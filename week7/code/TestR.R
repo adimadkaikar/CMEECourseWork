b4fe7307220c53923250fc8018a6458b82f644d9
adimadkaikar <arm122@ic.ac.uk>
2022-11-18 13:54:11 +0000

    Friday afternoon commit

---
+++ week7/code/TestR.R
@@ -0,0 +1,6 @@
+# Making the TestR file to use from subprocess
+
+print("Hello, this is R")
+print("This is the second line of prints")
+print("Trying this through subprocess now. Hopefully output gets updated")
+#print(purposely making a mistake)
