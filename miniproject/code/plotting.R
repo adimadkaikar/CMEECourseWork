333de122377f95c4125bab92fa833b070162ff1f
adimadkaikar <arm122@ic.ac.uk>
2022-11-18 16:47:06 +0000

    Forgot to save

--- miniproject/code/plotting.R
+++ miniproject/code/plotting.R
@@ -10,14 +10,11 @@ g <- g + facet_wrap(Species~., scales = "free")
 g
 
 pdf('../results/vis.pdf')
-for(ID_name in unique(data$Species)){
-    data_subset <- filter(data, Species == ID_name)
+for(ID_name in unique(data$ID)){
+    data_subset <- filter(data, ID == ID_name)
     g <- ggplot(data_subset, aes(x = Time, 
-                                 y = PopBio, 
-                                 colour = Temp,
-                                 shape = Citation)) + 
+                                 y = PopBio)) + 
         geom_point()
-    g <- g + facet_wrap(Medium~.) + theme(legend.position = "bottom")
     g <- g + xlab('Time') + ylab('Population') + ggtitle(ID_name)
     print(g) 
 }
